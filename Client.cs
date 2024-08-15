1f2ec7fee2552fa695b4297b15fde0010fa52594
Eugen Karaman <karamanjekakol@gmail.com>
2024-08-15 13:11:07 +0300

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client;
+class Client
+{
+    static void Main()
+    {
+        ConnectToServer();
+    }
+
+    static void ConnectToServer()
+    {
+        try
+        {
+            Console.Write("Requast is entered (time/date): ");
+            string userInput = Console.ReadLine();
+
+            TcpClient client = new TcpClient("127.0.0.1", 8888);
+            Console.WriteLine("Connecting to the server...");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = Encoding.ASCII.GetBytes(userInput);
+            stream.Write(data, 0, data.Length);
+
+            data = new byte[256];
+            StringBuilder responseData = new StringBuilder();
+            int bytes;
+
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                responseData.Append(Encoding.ASCII.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable);
+
+            Console.WriteLine("Server answer: " + responseData.ToString());
+
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR: " + ex.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using client;
+
+class Server
+{
+    static void Main()
+    {
+        StartServer();
+    }
+
+    public static void StartServer()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        TcpListener listener = new TcpListener(ipAddress, port);
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine("Server is started...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("New client is connected.");
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string request = Encoding.ASCII.GetString(buffer, 0, bytesRead).Trim();
+
+                string response = string.Empty;
+
+                if (request.ToLower() == "time")
+                {
+                    response = DateTime.Now.ToString("HH:mm:ss");
+                }
+                else if (request.ToLower() == "date")
+                {
+                    response = DateTime.Now.ToString("yyyy-MM-dd");
+                }
+                else
+                {
+                    response = "Invalid request";
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(response);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Sending answer: {response}");
+
+                client.Close();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR: " + ex.Message);
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
